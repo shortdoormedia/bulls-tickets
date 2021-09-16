c4fbc48018b4b6fe4d09688b1add35ac137203ea
shortdoormedia <61531750+shortdoormedia@users.noreply.github.com>
2021-09-15 21:48:32 -0500

    Move CSP to next.config.js file

---
+++ next.config.js
@@ -0,0 +1,38 @@
+const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: "default-src 'self' vitals.vercel-insights.com 'unsafe-eval' 'unsafe-inline'; img-src https://*; child-src 'none'; connect-src 'self' https://vitals.vercel-insights.com 'unsafe-eval' 'unsafe-inline' 'unsafe-hashes'; script-src 'self' vitals.vercel-insights.com 'unsafe-eval' 'unsafe-inline'"
+  }
+]
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
+}
